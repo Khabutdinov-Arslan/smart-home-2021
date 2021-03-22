536f9f34fb2fe11f2592941c757acd05c1a782f2
habutdinov arslan <habars2000@gmail.com>
2021-03-22 10:07:04 +0300

    Separated hall door close logic into another file

--- src/main/java/ru/sbt/mipt/oop/HallDoorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/HallDoorEventHandler.java
@@ -23,14 +23,16 @@ public class HallDoorEventHandler implements EventHandler{
             }
             for (Door door : room.getDoors()) {
                 if (door.getId().equals(event.getObjectId())) {
-                    if (event.getType() == DOOR_OPEN) {
-                        door.setOpen(true);
-                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
-                    } else {
-                        door.setOpen(false);
-                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
+                    if (event.getType() == DOOR_CLOSED) {
                         // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
                         // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
+                        for (Room homeRoom : smartHome.getRooms()) {
+                            for (Light light : homeRoom.getLights()) {
+                                light.setOn(false);
+                                SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
+                                commandPrinter.sendCommand(command);
+                            }
+                        }
                     }
                 }
             }
